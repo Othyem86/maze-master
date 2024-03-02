d851c12b0f69fcbabc67837858739d52ad251641
Othyem86 <emil.foger@gmail.com>
2024-03-02 12:18:47 +0100

    CHANGE: cells can now draw lines to other cells

--- gui/cell.py
+++ gui/cell.py
@@ -1,5 +1,6 @@
 from gui.basic_shapes import Point, Line
 from gui.window import Window
+from gui.cell import Cell
 
 class Cell:
     def __init__(self, win: Window) -> None:
@@ -7,26 +8,38 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self.__x1 = None
-        self.__y1 = None
-        self.__x2 = None
-        self.__y2 = None
-        self.__win = win
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = win
 
     def draw(self, x1: int, y1: int, x2: int, y2: int) -> None:
-        self.__x1 = x1
-        self.__y1 = y2
-        self.__x2 = x2
-        self.__y2 = y2
+        self._x1 = x1
+        self._y1 = y2
+        self._x2 = x2
+        self._y2 = y2
         if self.has_left_wall:
-            line = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
-            self.__win.draw_line(line)
+            line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            self._win.draw_line(line)
         if self.has_right_wall:
-            line = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
-            self.__win.draw_line(line)
+            line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            self._win.draw_line(line)
         if self.has_top_wall:
-            line = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
-            self.__win.draw_line(line)
+            line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            self._win.draw_line(line)
         if self.has_bottom_wall:
-            line = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
-            self.__win.draw_line(line)
+            line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            self._win.draw_line(line)
+
+    def draw_move(self, to_cell: Cell, undo:bool = False):
+        x_mid = (self._x1 + self._x2) / 2
+        y_mid = (self._y1 + self._y2) / 2
+        to_x_mid = (to_cell._x1 + to_cell._x2) / 2
+        to_y_mid = (to_cell._y1 + to_cell._y2) / 2
+        line_color = "gray" if undo else "red"
+        if y_mid == to_y_mid or x_mid == to_x_mid:
+            line = Line(Point(x_mid, y_mid), Point(to_x_mid, to_y_mid))
+            self._win.draw_line(line, line_color)
+        else:
+            raise ValueError("Invalid move, moves must be carthesian.")
